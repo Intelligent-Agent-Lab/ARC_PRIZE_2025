e005dd2874e5257075b1dfe5669b91a58281c08b
KukJinKim <ye20013@gmail.com>
2025-10-10 16:39:20 +0900

    remove unnecessary files

--- RL_ARC_AGI/mamba_layer.py
+++
@@ -1,6 +0,0 @@
-
-# In actor critic  
-# self.mamba_layers = MambaLayers(d_model=hidden_size,
-#                                 d_state=32,
-#                                 num_layers=3)
-        # shared_features = self.mamba_layers(x)
